bc957cacb4fd2451391cd1dadfa354dcb976a654
Peter Irvine <peirvine@gmail.com>
2021-04-28 21:03:48 -0500

    components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import Header from './components/header/header'
+import Biography from './components/biography/biography'
+import Sketches from './components/sketches/sketches'
+import Paintings from './components/paintings/paintings'
+import Contact from './components/contact/contact'
+import Footer from './components/footer/footer'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Biography />
+      <Sketches />
+      <Paintings />
+      <Contact />
+      <Footer />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/paintings/paintings.js
@@ -0,0 +1,3 @@
+export default function Paintings() {
+  return true
+}
