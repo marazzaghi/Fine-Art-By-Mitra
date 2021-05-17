336ce59eff8b898210559761a07592df1bd1529c
Peter Irvine <peirvine@gmail.com>
2021-05-16 22:16:50 -0500

    collapsed sections

--- src/components/paintings/paintings.js
+++ src/components/paintings/paintings.js
@@ -1,27 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './paintings.scss'
 import SimpleReactLightbox, { SRLWrapper } from "simple-react-lightbox-pro"
+import { Collapse } from 'react-bootstrap'
 
 import  { paintingsList, paintingsCaptions } from './paintingsList'
 
 export default function Paintings() {
+  const [open, setOpen] = useState(false)
   return (
     <div id="paintings">
-      <h2>Paintings</h2>
-      <div id="paintingsContent">
-        <SimpleReactLightbox>
-          <SRLWrapper customCaptions={paintingsCaptions}>
-            {paintingsList.length === 0 
-              ? "No Paintings Available"
-              : paintingsList.map(x => (
-                <a href={x.src} data-attribute="SRL">
-                  <img src={x.src} alt={x.alt}/>
-                </a>
-              ))
-            }
-          </SRLWrapper>
-        </SimpleReactLightbox>
-      </div>
+      <h2 onClick={() => setOpen(!open)}>Paintings</h2>
+      <Collapse in={open}>
+        <div id="paintingsContent">
+          <SimpleReactLightbox>
+            <SRLWrapper customCaptions={paintingsCaptions}>
+              {paintingsList.length === 0 
+                ? "No Paintings Available"
+                : paintingsList.map(x => (
+                  <a href={x.src} data-attribute="SRL">
+                    <img src={x.src} alt={x.alt}/>
+                  </a>
+                ))
+              }
+            </SRLWrapper>
+          </SimpleReactLightbox>
+        </div>
+      </Collapse>
     </div>
   )
 }
--- src/components/sketches/sketches.js
+++ src/components/sketches/sketches.js
@@ -1,27 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import SimpleReactLightbox, { SRLWrapper } from "simple-react-lightbox-pro"
+import { Collapse } from 'react-bootstrap'
 import './sketches.scss'
 
 import { sketchesList, sketchesCpations } from './sketchesList'
 
 export default function Sketches() {
+  const [open, setOpen] = useState(false)
   return (
     <div id="sketches">
-      <h2>Sketches</h2>
-      <div id="sketchesContent">
-        <SimpleReactLightbox>
-          <SRLWrapper customCaptions={sketchesCpations}>
-            {sketchesList.length === 0 
-              ? "No Sketches Available"
-              : sketchesList.map(x => (
-                <a href={x.src} data-attribute="SRL">
-                  <img src={x.src} alt={x.alt}/>
-                </a>
-              ))
-            }
-          </SRLWrapper>
-        </SimpleReactLightbox>
-      </div>
+      <h2 onClick={() => setOpen(!open)}>Sketches</h2>
+      <Collapse in={open}>
+        <div id="sketchesContent">
+          <SimpleReactLightbox>
+            <SRLWrapper customCaptions={sketchesCpations}>
+              {sketchesList.length === 0 
+                ? "No Sketches Available"
+                : sketchesList.map(x => (
+                  <a href={x.src} data-attribute="SRL">
+                    <img src={x.src} alt={x.alt}/>
+                  </a>
+                ))
+              }
+            </SRLWrapper>
+          </SimpleReactLightbox>
+        </div>
+      </Collapse>
     </div>
   )
 }
