13a255fa1ff7bd67b9d1cfb6286f102f0d382bbd
Peter Irvine <peirvine@gmail.com>
2021-05-24 22:09:16 -0500

    paypal

--- src/components/modalContents/modalContents.js
+++ src/components/modalContents/modalContents.js
@@ -1,8 +1,34 @@
-import React from 'react'
-import { Container, Row, Col, Modal } from 'react-bootstrap'
+import React, { useState } from 'react'
+import ReactDOM from "react-dom"
+import { Container, Row, Col, Modal, Alert } from 'react-bootstrap'
 import './modalContents.scss'
 
 export default function ModalContents(img) {
+  const PayPalButton = paypal.Buttons.driver("react", { React, ReactDOM })
+  const [alertOpen, setAlertOpen] = useState(false)
+  const [alertMessage, setAlertMessage] = useState('')
+  const [variant, setVariant] = useState('')
+
+  const createOrder = (data, actions) =>{
+    return actions.order.create({
+      purchase_units: [
+        {
+          amount: {
+            value: data.cost,
+          },
+          description: data.desc,
+        },
+      ],
+    })
+  }
+
+  const onApprove = (data, actions) => {
+    setAlertMessage('Art Purchased! Please check your email for a receipt')
+    setVariant('success')
+    setAlertOpen(true)
+    return actions.order.capture()
+  }
+  
   return(
     <>
       <Modal.Header closeButton>
@@ -17,15 +43,24 @@ export default function ModalContents(img) {
             <Col xs={12} md={6}>
               <h3>{img.title}</h3>
               <p><b>Medium:</b> {img.medium}</p>
-              {/* <p><b>Cost:</b> ${img.cost}</p> */}
+              <p><b>Cost:</b> ${img.cost}</p>
               <p><b>Artist Notes:</b> {img.description}</p>
             </Col>
           </Row>
         </Container>
       </Modal.Body>
-      {/* <Modal.Footer>
-        {img.buy}
-      </Modal.Footer> */}
+      <Modal.Footer>
+        <Alert show={alertOpen} variant={variant} id="alert" dismissible onClose={() => setAlertOpen(false)}>
+          <Alert.Heading />
+          <p>
+            {alertMessage}
+          </p>
+        </Alert>
+        <PayPalButton
+          createOrder={(data, actions) => createOrder({cost: img.cost, desc: img.title}, actions)}
+          onApprove={(data, actions) => onApprove(data, actions)}
+        />
+      </Modal.Footer>
     </>
   )
 }
